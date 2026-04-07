555ab7bd6e7c9b15b3f8909ae78114cfb701d774
Dixit <Dixit@gmail.com>
2026-04-07 22:20:08 +0530

    Initial commit for project-2

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+15:06 29-03-2026
+
+Testing auto Deployment using Trigger rather than webhooks
---
+++ devops-python-app/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASS"),
+        database="testdb"
+    )
+
+@app.route("/add", methods=["POST"])
+def add():
+    data = request.json
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (name VARCHAR(255))")
+    cursor.execute("INSERT INTO users (name) VALUES (%s)", (data["name"],))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"message": "User added"}
+
+@app.route("/get", methods=["GET"])
+def get():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+
+@app.route("/")
+def home():
+    return "DevOps Python App Running 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
